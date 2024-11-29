e3348d521dd50df3142572e2486bd488e4656c47
Muh. Zulhaidar Wahyudi <zulhaidarwahyudi@gmail.com>
2024-11-29 11:28:03 +0800

    Initial commit

---
+++ app/src/main/java/com/wahyuzul/flutter/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.wahyuzul.flutter
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.wahyuzul.flutter.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        setSupportActionBar(binding.toolbar)
+
+        supportFragmentManager.findFragmentById(R.id.fragment_main_activity)
+    }
+}
